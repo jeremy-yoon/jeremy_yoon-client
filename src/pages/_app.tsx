c43282ac0dade13756ed4e1f303d542a76a70a37
jeremy-yoon <coco@consalad.io>
2022-02-11 02:05:34 +0900

    메인 백그라운드 모션 추가, 커스텀 커서 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,23 @@ import "../styles/star-icon.css";
 import "antd/dist/antd.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import styles from "../styles/Home.module.css";
+import "styles/Cursor.module.css";
 import { Sv, St, MainHeader } from "components";
 import { useRouter } from "next/router";
 import styled, { css } from "styled-components";
 import { Layout, Menu, Breadcrumb, Row, Col } from "antd";
 import { RecoilRoot } from "recoil";
-import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
+import {
+  AnimatePresence,
+  domAnimation,
+  LazyMotion,
+  m,
+  motion,
+  useTransform,
+} from "framer-motion";
 import { animations } from "animations/animations";
-import react, { useState } from "react";
+import react, { useState, useRef } from "react";
+import useMouse from "@react-hook/mouse-position";
 
 const { Header, Content, Footer } = Layout;
 
@@ -24,6 +33,89 @@ const MyApp: React.FC<MyAppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
   const [animation, setAnimation] = useState(animations[startIndex]);
 
+  const [cursorText, setCursorText] = useState("");
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const ref = useRef(null);
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+
+  let mouseXPosition = 0;
+  let mouseYPosition = 0;
+
+  if (mouse.x !== null) {
+    mouseXPosition = mouse.clientX;
+  }
+
+  if (mouse.y !== null) {
+    mouseYPosition = mouse.clientY;
+  }
+
+  const variants = {
+    default: {
+      opacity: 1,
+      height: 10,
+      width: 10,
+      fontSize: "16px",
+      backgroundColor: "#1e91d6",
+      x: mouseXPosition,
+      y: mouseYPosition,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+    project: {
+      opacity: 1,
+      // backgroundColor: "rgba(255, 255, 255, 0.6)",
+      backgroundColor: "#fff",
+      color: "#000",
+      height: 80,
+      width: 80,
+      fontSize: "18px",
+      x: mouseXPosition - 32,
+      y: mouseYPosition - 32,
+    },
+    contact: {
+      opacity: 1,
+      backgroundColor: "#FFBCBC",
+      color: "#000",
+      height: 64,
+      width: 64,
+      fontSize: "32px",
+      x: mouseXPosition - 48,
+      y: mouseYPosition - 48,
+    },
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 28,
+  };
+
+  function projectEnter(event) {
+    setCursorText("View");
+    setCursorVariant("project");
+  }
+
+  function projectLeave(event) {
+    setCursorText("");
+    setCursorVariant("default");
+  }
+
+  function contactEnter(event) {
+    setCursorText("👋");
+    setCursorVariant("contact");
+  }
+
+  function contactLeave(event) {
+    setCursorText("");
+    setCursorVariant("default");
+  }
+
   return (
     <RecoilRoot>
       <Layout>
@@ -33,6 +125,7 @@ const MyApp: React.FC<MyAppProps> = ({ Component, pageProps }) => {
           <LazyMotion features={domAnimation}>
             <AnimatePresence exitBeforeEnter>
               <m.div
+                ref={ref}
                 key={router.route.concat(animation.name)}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -43,6 +136,23 @@ const MyApp: React.FC<MyAppProps> = ({ Component, pageProps }) => {
               >
                 <Component {...pageProps} />
               </m.div>
+              <motion.div
+                variants={variants}
+                className="circle"
+                animate={cursorVariant}
+                transition={spring}
+              >
+                <span className="cursorText">{cursorText}</span>
+                <div className="contactWrapper">
+                  <div
+                    className="contact"
+                    onMouseEnter={contactEnter}
+                    onMouseLeave={contactLeave}
+                  >
+                    Want to Chat?
+                  </div>
+                </div>
+              </motion.div>
             </AnimatePresence>
           </LazyMotion>
         </S.Content>
--- src/pages/home/Background.tsx
+++ src/pages/home/Background.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import { Sv, St, ButtonL, PostCardL, PostCardM, PostCardS } from "components";
+import useMouse from "@react-hook/mouse-position";
 import styled, { css, keyframes } from "styled-components";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import {
   bg,
   shape01,
@@ -17,40 +18,159 @@ import {
 } from "images/main";
 
 export default function Background() {
+  const x = useMotionValue(200);
+  const y = useMotionValue(200);
+
+  const rotateX = useTransform(y, [0, 1900], [45, -45]);
+  const rotateY = useTransform(x, [0, 1900], [-45, 45]);
+
+  function handleMouse(event) {
+    const rect = event.currentTarget.getBoundingClientRect();
+
+    x.set(event.clientX - rect.left);
+    y.set(event.clientY - rect.top);
+  }
+
+  ///////////////////////////////////
+
+  const [cursorText, setCursorText] = useState("");
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const ref = useRef(null);
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+
+  let mouseXPosition = 0;
+  let mouseYPosition = 0;
+
+  if (mouse.x !== null) {
+    mouseXPosition = mouse.clientX;
+  }
+
+  if (mouse.y !== null) {
+    mouseYPosition = mouse.clientY;
+  }
+
+  const variants = {
+    default: {
+      opacity: 1,
+      height: 10,
+      width: 10,
+      fontSize: "16px",
+      backgroundColor: "#1e91d6",
+      x: mouseXPosition,
+      y: mouseYPosition,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+    project: {
+      opacity: 1,
+      // backgroundColor: "rgba(255, 255, 255, 0.6)",
+      backgroundColor: "#fff",
+      color: "#000",
+      height: 80,
+      width: 80,
+      fontSize: "18px",
+      x: mouseXPosition - 32,
+      y: mouseYPosition - 32,
+    },
+    contact: {
+      opacity: 1,
+      backgroundColor: "#FFBCBC",
+      color: "#000",
+      height: 64,
+      width: 64,
+      fontSize: "32px",
+      x: mouseXPosition - 48,
+      y: mouseYPosition - 48,
+    },
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 28,
+  };
+
+  function projectEnter(event) {
+    setCursorText("View");
+    setCursorVariant("project");
+  }
+
+  function projectLeave(event) {
+    setCursorText("");
+    setCursorVariant("default");
+  }
+
+  function contactEnter(event) {
+    setCursorText("👋");
+    setCursorVariant("contact");
+  }
+
+  function contactLeave(event) {
+    setCursorText("");
+    setCursorVariant("default");
+  }
+
   return (
-    <S.BGContainer>
-      <Image src={bg} layout="fill" objectFit="cover" />
-      <S.Shape1_2>
+    <S.BGContainer onMouseMove={handleMouse}>
+      {/* <motion.div
+        variants={variants}
+        className="circle"
+        animate={cursorVariant}
+        transition={spring}
+      >
+        <span className="cursorText">{cursorText}</span>
+        <div className="contactWrapper">
+          <div
+            className="contact"
+            onMouseEnter={contactEnter}
+            onMouseLeave={contactLeave}
+          >
+            Want to Chat?
+          </div>
+        </div>
+      </motion.div> */}
+      {/* <Image src={bg} layout="fill" objectFit="cover" />
+
+      <S.Shape1_2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape01_2} width={1000} objectFit="scale-down" />
       </S.Shape1_2>
-      <S.Shape1>
+      <S.Shape1 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape01} width={1000} objectFit="scale-down" />
       </S.Shape1>
-      <S.Shape2_2>
+
+      <S.Shape2_2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape02_2} width={500} objectFit="scale-down" />
       </S.Shape2_2>
-      <S.Shape2>
+      <S.Shape2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape02} width={500} objectFit="scale-down" />
       </S.Shape2>
 
-      <S.Shape4_2>
+      <S.Shape4_2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape04_2} width={500} objectFit="scale-down" />
       </S.Shape4_2>
-      <S.Shape4>
+      <S.Shape4 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape04} width={500} objectFit="scale-down" />
       </S.Shape4>
-      <S.Shape5_2>
+
+      <S.Shape5_2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape05_2} width={1900} objectFit="scale-down" />
       </S.Shape5_2>
-      <S.Shape5>
+      <S.Shape5 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape05} width={1900} objectFit="scale-down" />
       </S.Shape5>
-      <S.Shape3_2>
+
+      <S.Shape3_2 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape03_2} width={800} objectFit="scale-down" />
       </S.Shape3_2>
-      <S.Shape3>
+      <S.Shape3 style={{ rotateX: rotateX, rotateY: rotateY }}>
         <Image src={shape03} width={800} objectFit="scale-down" />
-      </S.Shape3>
+      </S.Shape3> */}
     </S.BGContainer>
   );
 }
@@ -69,7 +189,14 @@ const slideUp = keyframes`
   }
 `;
 
-S.BGContainer = styled(Sv)`
+const globalStyle = css`
+  position: absolute;
+  mix-blend-mode: screen;
+  transition-duration: 3s;
+  perspective: 400;
+`;
+
+S.BGContainer = styled(motion.div)`
   position: absolute;
   left: 100px;
   right: 100px;
@@ -78,51 +205,46 @@ S.BGContainer = styled(Sv)`
   overflow: hidden;
 `;
 
-S.Shape1 = styled(Sv)`
-  position: absolute;
+S.Shape1 = styled(motion.div)`
+  ${globalStyle}
   top: -600px;
   right: -200px;
-  mix-blend-mode: screen;
 `;
 S.Shape1_2 = styled(S.Shape1)`
   mix-blend-mode: multiply !important;
 `;
 
-S.Shape2 = styled(Sv)`
-  position: absolute;
+S.Shape2 = styled(motion.div)`
+  ${globalStyle}
   top: -400px;
   left: 300px;
-  mix-blend-mode: screen;
 `;
 S.Shape2_2 = styled(S.Shape2)`
   mix-blend-mode: multiply !important;
 `;
 
-S.Shape3 = styled(Sv)`
-  position: absolute;
+S.Shape3 = styled(motion.div)`
+  ${globalStyle}
   bottom: -400px;
   left: 200px;
-  mix-blend-mode: screen;
 `;
 S.Shape3_2 = styled(S.Shape3)`
   mix-blend-mode: multiply !important;
 `;
 
-S.Shape4 = styled(Sv)`
-  position: absolute;
+S.Shape4 = styled(motion.div)`
+  ${globalStyle}
   bottom: -160px;
   right: 20px;
-  mix-blend-mode: screen;
 `;
 S.Shape4_2 = styled(S.Shape4)`
   mix-blend-mode: multiply !important;
 `;
 
-S.Shape5 = styled(Sv)`
-  position: absolute;
+S.Shape5 = styled(motion.div)`
+  ${globalStyle}
   bottom: -600px;
   left: -680px;
-  mix-blend-mode: screen;
 `;
 S.Shape5_2 = styled(S.Shape5)`
   mix-blend-mode: multiply !important;

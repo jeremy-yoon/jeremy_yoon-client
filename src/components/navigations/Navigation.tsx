73ee5b2e3cbca3aaa0058a80bdb5ac8ca2c136d4
jeremy-yoon <coco@consalad.io>
2022-03-07 00:27:06 +0900

    resume 버튼 추가

--- src/components/navigations/Navigation.tsx
+++ src/components/navigations/Navigation.tsx
@@ -3,15 +3,16 @@ import { Sv, St, NavigationButton } from "components";
 import styled, { css } from "styled-components";
 import ic_home from "images/svg/ic_home.svg";
 import ic_chat from "images/svg/ic_chat.svg";
+import ic_resume from "images/svg/ic_resume.svg";
 
 interface Navigation {}
 
 export const Navigation = () => {
   return (
     <S.Container col gy={12} jct>
-      <NavigationButton icon={ic_home} selected={true} />
-      <NavigationButton icon={ic_chat} />
-      <NavigationButton />
+      <NavigationButton icon={ic_home} selected={true} href="/blog" />
+      <NavigationButton icon={ic_chat} href="/guest-book" />
+      <NavigationButton icon={ic_resume} href="/resume" />
     </S.Container>
   );
 };

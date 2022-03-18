a63e9a0492bab131b5f49d13bb589299d1392bd0
jeremy-yoon <coco@consalad.io>
2022-03-18 23:19:21 +0900

    guestbook, resume 페이지 정리

--- src/pages/guest-book/index.tsx
+++ src/pages/guest-book/index.tsx
@@ -1,46 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { Sv, St, ProfileContainer, Navigation } from "components";
-import { Row, Col } from "antd";
-import styled from "styled-components";
+import { Sv, St, MainLogo, Comment, CommentInput } from "components";
+import styled, { css } from "styled-components";
 import { getRequest, postRequest } from "apis/common";
-import moment from "moment";
 import { colors } from "styles/colors";
-import parse from "html-react-parser";
-import { getPost } from "apis/service";
-import LeftContainer from "./LeftContainer";
 
-const PostScreen: React.FC = () => {
+const GuestBookScreen: React.FC = () => {
   const router = useRouter();
-  const { id } = router.query;
-
-  const [post, setPost] = useState();
-
-  const [isCommentModalOpen, setCommentModalOpen] = useState(undefined);
-
-  useEffect(() => {
-    if (id) {
-      getPost(setPost, id);
-    }
-  }, [id]);
 
   return (
-    <S.Body gx={16}>
-      <Navigation />
-      <LeftContainer />
-      <ProfileContainer bg={colors.g8} />
-    </S.Body>
+    <>
+      <S.Container>
+        <Sv px={120} col>
+          <MainLogo />
+          <Sv row mt={16}>
+            <St size={80} weight={200} text="안녕하세요." />
+          </Sv>
+          <St size={80} weight={800} text="방명록을" />
+          <St size={80} weight={200} text="작성해보세요." />
+        </Sv>
+      </S.Container>
+    </>
   );
 };
 
-export default PostScreen;
+export default GuestBookScreen;
 
 const S: any = {};
 
-S.Body = styled(Sv)`
-  background-color: ${colors.g6};
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+const blur = css`
+  background: ${colors.g8};
+  backdrop-filter: blur(30px);
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.05);
+  -webkit-backdrop-filter: blur(4px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+S.Container = styled(Sv)`
+  position: relative;
+  min-height: 100vh;
+  width: 1024px;
+  max-height: 100vh;
+  padding-top: 72px;
+  padding-bottom: 64px;
+  z-index: 1;
+  overflow-y: scroll;
+  ${blur}
+  ::-webkit-scrollbar {
+    width: 0px;
+    display: none;
+  }
+`;
+
+S.RepresentImage = styled(Image)``;
+
+S.BottomNavContainer = styled.div`
+  position: fixed;
+  bottom: 0;
 `;

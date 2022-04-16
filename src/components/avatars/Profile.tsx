4cf853d00b40888e2bb1ded99a0f4bfe4b96de2e
jeremy-yoon <coco@consalad.io>
2022-04-16 23:04:48 +0900

    프로필 업데이트

--- src/components/avatars/Profile.tsx
+++ src/components/avatars/Profile.tsx
@@ -19,7 +19,7 @@ export const Profile: React.FC<ProfileProps> = ({}) => {
         b2
         g3
         text={`효율적으로 일하는 방법을 고민하는 개발자 윤정탁 입니다.
-현재 스타트업에서 UI/UX 디자이너 겸 front-end 개발자로 일하고 있습니다. 
+현재 컨샐러드에서 프로덕트 디자이너 겸 프론트엔드 개발자로 일하고 있습니다. 
 `}
       />
       <Sv h={40} />

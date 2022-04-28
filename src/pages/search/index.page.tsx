e20fc11a927b3ca23606c77eed149c99cde6881f
jeremy-yoon <coco@consalad.io>
2022-04-28 23:05:35 +0900

    검색결과 페이지 레이아웃 수정

--- src/pages/search/index.page.tsx
+++ src/pages/search/index.page.tsx
@@ -16,23 +16,33 @@ const PortfolioScreen: React.FC = () => {
   }, [router]);
 
   return (
-    <>
-      <Sv>
-        <Sv px={120} col>
-          <Sv row mt={64}>
-            <St h2 weight={800} text={`${keyword}`} />
-            <St h2 weight={200} text="에 대한 검색결과" />
-          </Sv>
-          <Sv h={1} bg={colors.g5} my={20} />
-          {/* <PostList />
-          <PostList />
-          <PostList /> */}
+    <S.Container>
+      <Sv px={120} col>
+        <Sv row mt={64}>
+          <St h2 weight={800} text={`${keyword}`} />
+          <St h2 weight={200} text="에 대한 검색결과" />
         </Sv>
+        <Sv h={1} bg={colors.g5} my={20} />
       </Sv>
-    </>
+    </S.Container>
   );
 };
 
 export default PortfolioScreen;
 
 const S: any = {};
+
+S.Container = styled(Sv)`
+  position: relative;
+  width: 1024px;
+  max-height: 100vh;
+  padding-top: 72px;
+  padding-bottom: 64px;
+  z-index: 1;
+  overflow-y: scroll;
+  ${blur}
+  ::-webkit-scrollbar {
+    width: 0px;
+    display: none;
+  }
+`;

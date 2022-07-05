bdc956981f92741af307108be07d2b9620f937c7
jeremy-yoon <coco@consalad.io>
2022-07-05 23:51:58 +0900

    404 페이지 디자인 수정

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import { Sv, St } from "components";
+import { Sv, St, MainLogo } from "components";
 
 export default function FourOhFour() {
   return (
-    <Sv mt={64}>
+    <Sv>
+      <MainLogo />
       <St h1>404 - 페이지를 찾을 수 없어요 :(</St>
       <Link href="/">
         <Sv mt={20} pointer>
-          <St s1>홈 화면으로 가기 {`>`}</St>
+          <St s1>홈 화면으로 이동하기 {`>`}</St>
         </Sv>
       </Link>
     </Sv>

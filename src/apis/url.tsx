25c492809fc7e19ff004654f4e975f294d136014
jeremy-yoon <coco@consalad.io>
2022-08-31 23:40:00 +0900

    url address 변경

--- src/apis/url.tsx
+++ src/apis/url.tsx
@@ -1,11 +1,12 @@
 const development =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-var ADDRESS = "http://3.35.90.26:8000";
+var ADDRESS = "https://api.jeremy-yoon.com";
 
 if (development) {
-  ADDRESS = "http://localhost:8000";
+  // ADDRESS = "http://localhost:8000";
   // ADDRESS = "http://3.35.90.26:8000";
+  ADDRESS = "https://api.jeremy-yoon.com";
 }
 
 const ROOT_URL = ADDRESS;

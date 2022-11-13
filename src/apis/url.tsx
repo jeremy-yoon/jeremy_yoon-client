9b5404e95bec326179c8963310042945c283e7a4
jeremy-yoon <coco@consalad.io>
2022-11-13 21:47:36 +0900

    url 수정

--- src/apis/url.tsx
+++ src/apis/url.tsx
@@ -1,12 +1,12 @@
 const development =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-var ADDRESS = "https://api.jeremy-yoon.com";
+var ADDRESS = "http://api.jeremy-yoon.com";
 
 if (development) {
   // ADDRESS = "http://localhost:8000";
   // ADDRESS = "http://3.35.90.26:8000";
-  ADDRESS = "https://api.jeremy-yoon.com";
+  ADDRESS = "http://api.jeremy-yoon.com";
 }
 
 const ROOT_URL = ADDRESS;
